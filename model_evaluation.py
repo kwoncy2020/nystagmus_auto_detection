6001e10277a880a94456bf8d8506a80cad5e762c
kwoncy <beefbreaker2020@gmail.com>
2022-09-13 19:57:26 +0900

    eval_video_frames and csv plt

--- model_evaluation.py
+++ model_evaluation.py
@@ -44,9 +44,36 @@ def model_evaluate_with_predicted_images2(folder_path:str)->dict:
     
     pred_npimgs_binary = pred_npimgs / 255.
     mask_npimgs_binary = mask_npimgs / 255.
+    
+    dice2_scores = []
+    dice3_scores = []
+    image_size = mask_npimgs[0].size
+    for index in range(len(mask_npimgs_binary)):
+        temp_mask = mask_npimgs_binary[index]
+        temp_pred = pred_npimgs_binary[index]
+        mask_area = np.sum(temp_mask)
+        pred_area = np.sum(temp_pred)
+        if mask_area == 0 and pred_area == 0:
+            dice2_scores.append(1)
+        elif mask_area == 0 and pred_area != 0:
+            dice2_scores.append(1)
+        else:
+            dice2_scores.append(2*np.sum(temp_mask*temp_pred) / (mask_area+pred_area))
+
+        intersection = temp_mask==temp_pred
+        dice3_scores.append(2*np.sum(intersection)/(2*(image_size)))
 
 
-    tn, fp, fn, tp  = sklearn.metrics.confusion_matrix(mask_npimgs_binary.flatten(),pred_npimgs_binary.flatten()).ravel()
+    print("dice2:",np.sum(dice2_scores)/len(dice2_scores))
+    print("dice3:",np.sum(dice3_scores)/len(dice3_scores))
+    
+    mask_npimgs_flatten = mask_npimgs_binary.flatten()
+    pred_npimgs_flatten = pred_npimgs_binary.flatten()
+
+    dice = 2*np.sum(mask_npimgs_flatten*pred_npimgs_flatten)/(np.sum(mask_npimgs_flatten)+np.sum(pred_npimgs_flatten))
+    
+    tn, fp, fn, tp  = sklearn.metrics.confusion_matrix(mask_npimgs_flatten,pred_npimgs_flatten).ravel()
+
     print(" tn: ",tn," fp: ",fp," fn: ",fn," tp: ",tp)
     sensitivity = tp/(tp+fn)
     print("Sensitivity: ", sensitivity)
@@ -58,7 +85,10 @@ def model_evaluate_with_predicted_images2(folder_path:str)->dict:
     print("Accuracy: ", accuracy)
     f1_score = 2*sensitivity*precision/(sensitivity+precision)
     print("F1-score: ", f1_score)
+    print("dice_score: ", dice)
 
+
+    return {"TN":tn, "FP":fp, "FN":fn, "TP":tp, "Accuracy":accuracy, "F1-score":f1_score, "dice_score":dice}
 
 ## this function evaluate model with not whole image's differencies [0,1], only IOU area
 def model_evaluate_with_predicted_images(folder_path:str)->dict:
@@ -85,6 +115,9 @@ def model_evaluate_with_predicted_images(folder_path:str)->dict:
 
     AP_score = get_average_precision_score(y_true, y_pred, flag_draw=True)
     print(AP_score)
+    confusion_matrix["AP_score"] = AP_score
+    
+    return confusion_matrix
 
 ## the folder must have like below condition.
 ## index_orig.png , index_mask.png
@@ -92,11 +125,14 @@ def check_folder_predicted_images(folder_path:str) -> bool:
     orig_file_list = glob.glob(f'{folder_path}/*_orig.png')
     mask_file_list = glob.glob(f'{folder_path}/*_mask.png')
     pred_file_list = glob.glob(f'{folder_path}/*_predicted.png')
+    
     if len(pred_file_list) != len(mask_file_list) or len(mask_file_list) != len(orig_file_list):
         print("len_orig_file: ",len(orig_file_list))
         print("len_mask_file: ",len(mask_file_list))
         print("len_pred_file: ",len(pred_file_list))
         return False
+    if len(orig_file_list) == 0:
+        raise Exception("from check_folder_predicted_images: couldn't find orig files.")
     return True
 
 def make_predicted_images_to_folder(folder_path:str):
@@ -130,18 +166,22 @@ def make_predicted_images_to_folder(folder_path:str):
 def load_pred_gray_and_mask_gray_imgs(folder_path:str)->'list[np.ndarray, np.ndarray, np.ndarray]':
     mask_file_list = glob.glob(f'{folder_path}/*_mask.png')
     re_mask = re.compile('([0-9]+)_mask.png')
-    mask_imgs = []
-    pred_imgs = []
-    indices = []
-
+    
+    
+    results = []
+    
     for mask_file in mask_file_list:
         head, tail = os.path.split(mask_file)
         num_str = re_mask.match(tail).group(1)
-        indices.append(int(num_str))
         pred_file = os.path.join(head, num_str + "_predicted.png")
 
-        mask_imgs.append(cv2.imread(mask_file,cv2.IMREAD_GRAYSCALE))
-        pred_imgs.append(cv2.imread(pred_file,cv2.IMREAD_GRAYSCALE))
+        num_str = int(num_str)
+        results.append((num_str,cv2.imread(mask_file,cv2.IMREAD_GRAYSCALE),cv2.imread(pred_file,cv2.IMREAD_GRAYSCALE)))
+
+    results = sorted(results, key=lambda x:x[0])
+    indices = [i[0] for i in results]
+    pred_imgs = [i[2] for i in results]
+    mask_imgs = [i[1] for i in results]
 
     return [np.array(indices), np.array(pred_imgs), np.array(mask_imgs)]
 
@@ -194,12 +234,12 @@ def get_mask_and_pred_centers_with_index(folder_path:str, roundness:int=None)->'
     return [list(indices), mask_x_list, mask_y_list, pred_x_list, pred_y_list]
 
 
-def save_indexed_centers_csv(folder_path:str, roundness:int=None)->None:
+def get_centers_rmse(folder_path:str, roundness:int=None, flag_save_to_csv:bool=False, flag_show_plt:bool=False)-> 'list[float, np.ndarray, np.ndarray, np.ndarray, np.ndarray]':
     indices, mask_x_list, mask_y_list, pred_x_list, pred_y_list = get_mask_and_pred_centers_with_index(folder_path,roundness)
     fe = FeatureExtractor()
     indices = np.array(indices)
     
-    ## ex) grouped_indices = [[100,850], [1000,1750]]
+    ## ex) make consecutive numbers to be grouped_indices = [[100,850], [1000,1750]]
     grouped_indices = fe.get_grouped_sequence(indices)
     
     npa_mask_x = []
@@ -209,19 +249,80 @@ def save_indexed_centers_csv(folder_path:str, roundness:int=None)->None:
 
     for grouped in grouped_indices:
         start_num, end_num = grouped
-        start_index = np.where(indices==start_num)[0]
-        end_index = np.where(indices==end_num)[0]
+        start_index = int(np.where(indices==start_num)[0])
+        end_index = int(np.where(indices==end_num)[0])
         npa_mask_x = np.append(npa_mask_x, np.array(fe.fill_na(mask_x_list[start_index:end_index+1],'all')))
         npa_mask_y = np.append(npa_mask_y, np.array(fe.fill_na(mask_y_list[start_index:end_index+1],'all')))
         npa_pred_x = np.append(npa_pred_x, np.array(fe.fill_na(pred_x_list[start_index:end_index+1],'all')))
         npa_pred_y = np.append(npa_pred_y, np.array(fe.fill_na(pred_y_list[start_index:end_index+1],'all')))
 
-    with open(f'{os.path.join(folder_path,"indexed_centers").csv}','w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["index", "mask_x", "mask_y", "pred_x", "pred_y"])
-        for i in range(len(indices)):
-            writer.writerow([indices[i], npa_mask_x[i], npa_mask_y[i], npa_pred_x[i], npa_pred_y[i]])
+    x_diff_squares = np.square(npa_mask_x - npa_pred_x)
+    y_diff_squares = np.square(npa_mask_y - npa_pred_y)
+
+    distances_squeares = x_diff_squares + y_diff_squares
+    if len(distances_squeares) == 0:
+        rmse = 0
+    else:
+        rmse = np.sqrt(np.sum(distances_squeares) / len(distances_squeares))
+    
+    print("rmse:",rmse)
+
+    if flag_save_to_csv:
+        with open(f'{os.path.join(folder_path,"indexed_centers")}.csv','w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["index", "mask_x", "mask_y", "pred_x", "pred_y"])
+            for i in range(len(indices)):
+                writer.writerow([indices[i], npa_mask_x[i], npa_mask_y[i], npa_pred_x[i], npa_pred_y[i]])
+
+
+
+    if flag_show_plt:
+        # fig, axes = plt.subplots(2,5, figsize=(2000,1000))
+        
+        frames_step = 150
+        a = np.arange(0,750,150)
+        b = np.arange(751,1500,150)
+        start_indices = np.append(a,b)
+        
+        # fig = plt.figure()
+        # # plt.plot(npa_mask_x[:750],'g-')
+        # # plt.plot(npa_mask_y[:750],'g-')
+        # # plt.plot(npa_pred_x[:750],'r--')
+        # # plt.plot(npa_pred_y[:750],'r--')
+
+        # # plt.plot(npa_mask_x[752:],'g-')
+        # plt.plot(npa_mask_y[752:],'g-')
+        # # plt.plot(npa_pred_x[752:],'r--')
+        # plt.plot(npa_pred_y[752:],'r--')
+
+        # plt.show()
+
+        # for i in start_indices:
+        #     fig = plt.figure()
+        #     plt.plot(range(i,i+frames_step), npa_mask_x[i:i+frames_step], 'g-')
+        #     # plt.plot(range(i,i+frames_step), npa_mask_y[i:i+frames_step], 'g-')
+        #     plt.plot(range(i,i+frames_step), npa_pred_x[i:i+frames_step], 'r--')
+        #     # plt.plot(range(i,i+frames_step), npa_pred_y[i:i+frames_step], 'r--')
+        #     plt.show()
             
+        # row = 0
+        # col = 0 
+
+        # for i in start_indices:
+        #     if col == 5:
+        #         row = 1
+        #         col = 0
+        #     axes[row,col].plot(range(i,i+frames_step), npa_mask_x[i:i+frames_step], 'g-')
+        #     # axes[row,col].plot(range(i,i+frames_step), npa_mask_y[i:i+frames_step], 'g-')
+        #     axes[row,col].plot(range(i,i+frames_step), npa_pred_x[i:i+frames_step], 'r--')
+        #     # axes[row,col].plot(range(i,i+frames_step), npa_pred_y[i:i+frames_step], 'r--')
+        #     col += 1
+
+        # plt.show()
+
+    return [rmse, npa_mask_x, npa_mask_y, npa_pred_x, npa_pred_y]
+
+
 def get_IOUs(pred_imgs:np.ndarray, mask_imgs:np.ndarray)->np.ndarray:
     
     IOUs = []
@@ -292,8 +393,6 @@ def get_average_precision_score(y_true:np.ndarray, y_pred:np.ndarray, flag_draw:
     return average_precision
 
 
-
-
 ## copy images from from_folder to to_folder. the index is accumulated
 def copy_images(from_folder:str, to_folder:str, flag_predicted_image:bool=True)-> None:
     from_images = glob.glob(os.path.join(from_folder,"*_orig.png"))
@@ -313,12 +412,18 @@ def copy_images(from_folder:str, to_folder:str, flag_predicted_image:bool=True)-
             
 if __name__ == '__main__':
     # f_path = "C:\kwoncy\eye\image_datas\\6_sixth_make_roi_with_bad_video_images\\test_with_clean_little_bad_imgs"
-    f_path = "C:\kwoncy\eye\image_datas\\6_sixth_make_roi_with_bad_video_images\\test_with_bad_imgs"
+    # f_path = "C:\kwoncy\eye\image_datas\\6_sixth_make_roi_with_bad_video_images\\test_with_bad_imgs"
+    
+    # f_path = "C:/kwoncy/eye/image_datas/consecutive_rois/01372181_KwonSanggyun_Caloric Lt cool_Acute Peripheral Vestibulopathy[L]/Lt_all"
+    # f_path = "C:/kwoncy/eye/image_datas/consecutive_rois/kwonsanggyun/Lt_all"
+    # f_path = "C:/kwoncy/eye/image_datas/consecutive_rois/kimjeongsim/Lt_all"
+    f_path = "C:/kwoncy/eye/image_datas/consecutive_rois/chasabong/Lt_all"
+    
     # model_evaluate_with_predicted_images(f_path)
-    model_evaluate_with_predicted_images2(f_path)
+    # model_evaluate_with_predicted_images2(f_path)
+    get_centers_rmse(f_path,0.6,flag_save_to_csv=False,flag_show_plt=True)
+
     # FROM_FOLDER = "C:\kwoncy\eye\image_datas\\6_sixth_make_roi_with_bad_video_images\\test_with_little_bad_imgs" 
     # TO_FOLDER = "C:\kwoncy\eye\image_datas\\6_sixth_make_roi_with_bad_video_images\\test_with_clean_little_bad_imgs" 
     # copy_images(FROM_FOLDER,TO_FOLDER)
-    
-    # print(2*0.990*0.977/(0.990+0.977))
-    # print(2*0.835*0.957/(0.835+0.957))
+
